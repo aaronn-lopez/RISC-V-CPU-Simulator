4056c4d08af1a306a6960af3249da70703079e97
MattYu2004 <88687501+MattYu2004@users.noreply.github.com>
2024-07-29 21:34:12 -0700

    Update utils.h

--- singlefetch/utils.h
+++ singlefetch/utils.h
@@ -1,184 +1,26 @@
-#ifndef __PIPELINE_H__
-#define __PIPELINE_H__
+#ifndef __UTILS_H__
+#define __UTILS_H__
 
-#include "config.h"
 #include "types.h"
-#include "cache.h"
-#include <stdbool.h>
 
-///////////////////////////////////////////////////////////////////////////////
-/// Functionality
-///////////////////////////////////////////////////////////////////////////////
+#define RTYPE_FORMAT "%s\tx%d, x%d, x%d\n"
+#define ITYPE_FORMAT "%s\tx%d, x%d, %d\n"
+#define MEM_FORMAT "%s\tx%d, %d(x%d)\n"
+#define LUI_FORMAT "lui\tx%d, %d\n"
+#define JAL_FORMAT "jal\tx%d, %d\n"
+#define BRANCH_FORMAT "%s\tx%d, x%d, %d\n"
+#define ECALL_FORMAT "ecall\n"
+#define CACHE_EVICTION_FORMAT "[MEM]: Cache eviction for address: 0x%.8llx\n"
+#define CACHE_HIT_FORMAT "[MEM]: Cache hit for address: 0x%.8llx\n"
+#define CACHE_MISS_FORMAT "[MEM]: Cache miss for address: 0x%.8llx\n"
 
-extern simulator_config_t sim_config;
-extern uint64_t miss_count;
-extern uint64_t hit_count;
-extern uint64_t total_cycle_counter;
-extern uint64_t mem_access_counter;
-extern uint64_t stall_counter;
-extern uint64_t branch_counter;
-extern uint64_t fwd_exex_counter;
-extern uint64_t fwd_exmem_counter;
+Instruction parse_instruction(uint32_t);
+int sign_extend_number(unsigned, unsigned);
+int get_branch_offset(Instruction);
+int get_jump_offset(Instruction);
+int get_store_offset(Instruction);
+void handle_invalid_instruction(Instruction);
+void handle_invalid_read(Address);
+void handle_invalid_write(Address);
 
-///////////////////////////////////////////////////////////////////////////////
-/// RISC-V Pipeline Register Types
-///////////////////////////////////////////////////////////////////////////////
-
-typedef struct
-{
-  Instruction instr;
-  uint32_t    instr_addr;
-  uint8_t rs1;
-  uint8_t rs2; 
-}ifid_reg_t;
-
-typedef struct
-{
-  Instruction instr;
-  uint32_t    instr_addr;
-  uint32_t rs1_val;
-  uint32_t rs2_val;
-  uint32_t imm;
-  uint32_t funct3;
-  uint32_t funct7;
-  uint32_t rd;  
-  uint32_t ALU_in_1;
-  uint32_t ALU_in_2;
-  uint32_t alu_control;
-  uint8_t rs1;
-  uint8_t rs2;
-  uint8_t ALUOp;
-  uint32_t Write_Address;
-  bool ALUSrc;
-  bool Branch;
-  bool Mem_Read;
-  bool Mem_Write;
-  bool Memto_Reg;
-  bool Reg_Write;
-}idex_reg_t;
-
-typedef struct
-{
-  Instruction instr;
-  uint32_t    instr_addr;
-  uint32_t    instr_addr_imm;
-  uint32_t Read_Address;
-  uint32_t Write_Address;
-  uint32_t funct3;
-  uint32_t contents;
-  uint8_t rd;
-  uint32_t rs1_val;
-  uint32_t rs2_val;
-  uint32_t imm;
-  bool zero;
-  bool Branch;
-  bool Mem_Read;
-  bool Mem_Write;
-  bool Memto_Reg;
-  bool Reg_Write;
-}exmem_reg_t;
-
-typedef struct
-{
-  Instruction instr;
-  uint32_t instr_addr;
-  uint32_t instr_addr_imm;
-  uint32_t Read_Data;
-  uint32_t Read_Address;
-  uint32_t Write_Data;
-  uint8_t rd;
-  uint32_t rs1_val;
-  uint32_t rs2_val;
-  uint32_t imm;
-  bool Memto_Reg;
-  bool Reg_Write;
-  bool Mem_Read;
-}memwb_reg_t;
-
-///////////////////////////////////////////////////////////////////////////////
-/// Register types with input and output variants for simulator
-///////////////////////////////////////////////////////////////////////////////
-
-typedef struct
-{
-  ifid_reg_t inp;
-  ifid_reg_t out;
-}ifid_reg_pair_t;
-
-typedef struct
-{
-  idex_reg_t inp;
-  idex_reg_t out;
-}idex_reg_pair_t;
-
-typedef struct
-{
-  exmem_reg_t inp;
-  exmem_reg_t out;
-}exmem_reg_pair_t;
-
-typedef struct
-{
-  memwb_reg_t inp;
-  memwb_reg_t out;
-}memwb_reg_pair_t;
-
-///////////////////////////////////////////////////////////////////////////////
-/// Functional pipeline requirements
-///////////////////////////////////////////////////////////////////////////////
-
-typedef struct
-{
-  ifid_reg_pair_t  ifid_preg;
-  idex_reg_pair_t  idex_preg;
-  exmem_reg_pair_t exmem_preg;
-  memwb_reg_pair_t memwb_preg;
-}pipeline_regs_t;
-
-typedef struct
-{
-  bool      pcsrc;
-  uint32_t  pc_src0;
-  uint32_t  pc_src1;
-  uint32_t forwardA;
-  uint32_t forwardB;
-  bool PCWriteHZD;
-  bool IFIDWriteHZD;
-  bool ControlMUXHZD;
-}pipeline_wires_t;
-
-
-///////////////////////////////////////////////////////////////////////////////
-/// Function definitions for different stages
-///////////////////////////////////////////////////////////////////////////////
-
-/**
- * output : ifid_reg_t
- **/ 
-ifid_reg_t stage_fetch(pipeline_wires_t* pwires_p, regfile_t* regfile_p, Byte* memory_p);
-
-/**
- * output : idex_reg_t
- **/ 
-idex_reg_t stage_decode(ifid_reg_t ifid_reg, pipeline_wires_t* pwires_p, regfile_t* regfile_p);
-
-/**
- * output : exmem_reg_t
- **/ 
-exmem_reg_t stage_execute(idex_reg_t idex_reg, pipeline_wires_t* pwires_p);
-
-/**
- * output : memwb_reg_t
- **/ 
-memwb_reg_t stage_mem(exmem_reg_t exmem_reg, pipeline_wires_t* pwires_p, Byte* memory, Cache* cache_p);
-
-/**
- * output : write_data
- **/ 
-void stage_writeback(memwb_reg_t memwb_reg, pipeline_wires_t* pwires_p, regfile_t* regfile_p);
-
-void cycle_pipeline(regfile_t* regfile_p, Byte* memory_p, Cache* cache_p, pipeline_regs_t* pregs_p, pipeline_wires_t* pwires_p, bool* ecall_exit);
-
-void bootstrap(pipeline_wires_t* pwires_p, pipeline_regs_t* pregs_p, regfile_t* regfile_p);
-
-#endif  // __PIPELINE_H__
+#endif // __UTILS_H__
